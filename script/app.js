7201437968e97107dec858a4e414633d6e3e4499
ljmcdade <ljmcdade@gmail.com>
2015-12-10 21:21:05 +0000

    exercise-1-hiding-and-showing-announcements

--- script/app.js
+++ script/app.js
@@ -1,1 +1,21 @@
-// Write your code here!
+// // Write your code here!
+$(document).ready(function() {
+
+    var $close = $("<button class='closeButton'>X</button>");
+
+    $(".announcement").prepend($close);
+
+    $('.closeButton').on("click", function() {
+        
+        $(this).parent().find("p").toggle("slow");
+
+        if ($(this).text() === "X"){
+
+        $(this).text("show announcement");
+        }
+        else {
+            $(this).text("X");
+        }
+    });
+});
+
