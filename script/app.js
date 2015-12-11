58a8e8600a0c415fed198dbbc8c0d2d41410778d
ljmcdade <ljmcdade@gmail.com>
2015-12-11 01:14:49 +0000

    exercise-3-discovering-forms

--- script/app.js
+++ script/app.js
@@ -1,1 +1,23 @@
 // Write your code here!
+
+$(document).ready(function() {
+
+    $('#signup-form').on('submit', function(event) {
+        // write your code here
+        
+        var password = $('input[name=password]').val();
+        var confirmPassword = $('input[name=password2]').val();
+        
+        if (password !== confirmPassword){
+            alert("Passwords don't match!");
+        }
+        else if (password.length < 10) {
+            alert("Password is too short!");
+        }
+        else {
+            return;
+        }
+        
+        event.preventDefault();
+    });
+});
